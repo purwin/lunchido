93fab39c75622476e32992ecb14057cd5008e4d2
Michael Purwin <mpurwin@gmail.com>
2018-08-25 10:11:53 -0400

    Init maps.js

---
+++ maps.js
@@ -0,0 +1,31 @@
+get start point
+
+get first rando open place
+
+determine if it's within 15 mins
+
+get:
+name,
+location,
+food type,
+price,
+reviews,
+photos,
+distance in time
+
+add it to list
+
+if perfect:
+give directions
+
+if not:
+  update search params
+  run search function
+
+
+
+
+search params (updated throughout):
+maxPrice (3 at first)
+excludeType (empty at first)
+maxDistance (15min at first)
