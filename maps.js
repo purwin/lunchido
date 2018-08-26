daf881cfb051f9345de7a4930c866ad0a030406b
Michael Purwin <mpurwin@gmail.com>
2018-08-26 09:39:27 -0400

    Add geocode function

--- maps.js
+++ maps.js
@@ -1,31 +1,184 @@
-get start point
+// get start point
 
-get first rando open place
+// get first rando open place
 
-determine if it's within 15 mins
+// determine if it's within 15 mins
 
-get:
-name,
-location,
-food type,
-price,
-reviews,
-photos,
-distance in time
+// get:
+// name,
+// location,
+// food type,
+// price,
+// reviews,
+// photos,
+// distance in time
 
-add it to list
+// add it to list
 
-if perfect:
-give directions
+// if perfect:
+// give directions
 
-if not:
-  update search params
-  run search function
-
-
+// if not:
+//   update search params
+//   run search function
 
 
-search params (updated throughout):
-maxPrice (3 at first)
-excludeType (empty at first)
-maxDistance (15min at first)
+
+
+// search params (updated throughout):
+// maxPrice (3 at first)
+// excludeType (empty at first)
+// maxDistance (15min at first)
+
+
+
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, infoWindow;
+
+function initMap() {
+  var styles = [
+    {
+      "featureType": "poi",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.business",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ];
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 39.952583, lng: -75.165222},
+    zoom: 15,
+    mapTypeControl: false,
+    styles: styles
+  });
+  infoWindow = new google.maps.InfoWindow;
+}
+
+function manualLocateMe(address) {
+  // Initialize the geocoder.
+  var geocoder = new google.maps.Geocoder();
+  // Make sure the address isn't blank.
+  if (address == '') {
+    window.alert('You gotta enter an address or location!');
+  } else {
+    // Geocode the address/area entered to get the center. Then, center the map
+    // on it and zoom in
+    geocoder.geocode(
+      { address: address,
+      }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          map.setZoom(15);
+          // Store lat/lng coordinates
+          var lunchCoordinates = {
+            lat: results[0].geometry.location.lat(),
+            long: results[0].geometry.location.lng()
+          }
+        } else {
+          window.alert('Couldn’t find that location. Try being more specific.');
+        }
+      });
+  }
+}
+
+function geolocateMe() {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Let’s work from here!');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
+
+// Adapted from Udacity https://github.com/udacity/ud864
+// This function allows the user to input a desired travel time, in
+// minutes, and a travel mode, and a location - and only show the listings
+// that are within that travel time (via that travel mode) of the location
+function searchWithinTime() {
+  // Initialize the distance matrix service.
+  var distanceMatrixService = new google.maps.DistanceMatrixService;
+  var address = document.getElementById('search-within-time-text').value;
+  // Check to make sure the place entered isn't blank.
+  if (address == '') {
+    window.alert('You must enter an address.');
+  } else {
+    hideListings();
+    // Use the distance matrix service to calculate the duration of the
+    // routes between all our markers, and the destination address entered
+    // by the user. Then put all the origins into an origin matrix.
+    var origins = [];
+    for (var i = 0; i < markers.length; i++) {
+      origins[i] = markers[i].position;
+    }
+    var destination = address;
+    var mode = document.getElementById('mode').value;
+    // Now that both the origins and destination are defined, get all the
+    // info for the distances between them.
+    distanceMatrixService.getDistanceMatrix({
+      origins: origins,
+      destinations: [destination],
+      travelMode: google.maps.TravelMode[mode],
+      unitSystem: google.maps.UnitSystem.IMPERIAL,
+    }, function(response, status) {
+      if (status !== google.maps.DistanceMatrixStatus.OK) {
+        window.alert('Error was: ' + status);
+      } else {
+        displayMarkersWithinTime(response);
+      }
+    });
+  }
+}
