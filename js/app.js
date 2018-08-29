98c022186f0439d3f24607311d90671527318dc4
Michael Purwin <mpurwin@gmail.com>
2018-08-28 23:19:52 -0400

    Add update parameter functions

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
 // Knockout.js file
 
-// model:
-//   startingPoint
-//   lunchOptions
-//   savedPlaces
-//   searchParams
-// initialLunch = [
-// {
-//   name: "Place 1",
-//   location: {
-//     lat: 40.7579854,
-//     lng: -73.8829541
-//   }
-// ];
 
 var ViewModel = function() {
+
+  // Stored FourSquare API info
+  this.fourSquareAPI = {
+    url: "https://api.foursquare.com/v2/venues/",
+    client_id: "RGJIQWJAUURIHR1SSJUTQFKQYXL3RFO34MFEWPBBAFUALZ2B",
+    client_secret: "2CA2UTR0RRAGVLIDRSUL20XK4YHY554Z0UKYYZCQ0IVLJKCA"
+  }
 
   var self = this;
 
@@ -28,7 +22,8 @@ var ViewModel = function() {
   // General exclusion parameters
   this.excludeParams = ko.observable({
     distance: "",
-    types: [] 
+    types: [],
+    price: 3
   })
 
   // Array of potential lunch options pulled from the FourSquare API
@@ -46,7 +41,7 @@ var ViewModel = function() {
   // Function to use geolocation for Google Maps starting point
   this.geoLocater = function() {
     var x = geolocateMe();
-    console.log(x);
+    console.log("GEOLOCATE RETURN: " + x);
     self.startingPoint(x);
   }
 
@@ -54,7 +49,7 @@ var ViewModel = function() {
   this.manualLocater = function() {
     console.log(this.manualSearch());
     var x = manualLocateMe(this.manualSearch());
-    console.log(x);
+    console.log("MANUAL RETURN: " + x);
     this.startingPoint(x);
   }
 
@@ -65,22 +60,24 @@ var ViewModel = function() {
   };
 
   // Function to highlight selected lunch option in list, show location on the map
-  this.selectedSpot = function(selected) {
+  this.selectSpot = function(selected) {
     // set current lunch item to clicked item
-    self.currentSpot(selected.id);
+    self.currentSpot(selected);
+    console.log("currentSpot: " + selected);
+    console.log("currentSpot ID: " + self.currentSpot().id);
+    console.log("currentSpot price: " + self.currentSpot().price);
     // Run map function to show relevant marker and info window
     selectLunch(selected);
   }
 
   // Function to pull lunch options from FourSquare API
   this.getLunchData = function() {
-    var fourSquareAPI = "https://api.foursquare.com/v2/venues/explore";
     // JSON request
     console.log("Searching " + self.startingPoint().lat.toString() + ", " + self.startingPoint().lng.toString());
-    $.getJSON( fourSquareAPI, {
+    $.getJSON( self.fourSquareAPI.url + "explore", {
       // Pull relevant data from the API
-      client_id: "RGJIQWJAUURIHR1SSJUTQFKQYXL3RFO34MFEWPBBAFUALZ2B",
-      client_secret: "2CA2UTR0RRAGVLIDRSUL20XK4YHY554Z0UKYYZCQ0IVLJKCA",
+      client_id: self.fourSquareAPI.client_id,
+      client_secret: self.fourSquareAPI.client_secret,
       ll: self.startingPoint().lat + "," + self.startingPoint().lng,
       v: '20180827',
       query: "lunch",
@@ -100,7 +97,8 @@ var ViewModel = function() {
             lng: item.venue.location.lng,
           },
           name: item.venue.name,
-          shortAddress: item.venue.location.address
+          shortAddress: item.venue.location.address,
+          price: null
           });
       });
       self.getLunch();
@@ -111,19 +109,40 @@ var ViewModel = function() {
     });
   }
 
-  // Function to get a lunch option, add it to observableArray
+  // Function to scrape a lunch option, add it to observableArray
   this.getLunch = function() {
     if (self.lunchSearch().length > 0 && self.lunchList().length < 5) {
       // Get random lunch spot from search array, remove from array
       lunchItem = self.lunchSearch.splice(Math.floor(Math.random()*self.lunchSearch().length), 1);
       // Apply id to lunch item
       lunchItem[0].id = self.lunchList().length + 1;
+      // Get price parameter from FourSquare API
+      $.getJSON( self.fourSquareAPI.url + lunchItem[0].venueID, {
+        // Query FourSquare API to get venue info (price)
+        client_id: self.fourSquareAPI.client_id,
+        client_secret: self.fourSquareAPI.client_secret,
+        v: '20180827',
+        format: "json"
+      })
+      .done(function(data) {
+        try {
+          // Pull price data from the API, add to object
+          lunchItem[0].price = data.response.venue.price.tier;
+        }
+        catch(error) {
+          console.log(error);
+        }
+      })
+      .fail(function(jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+      });
       // Add lunch spot to option list
       self.lunchList.push(lunchItem[0]);
       // Run create map marker function
       addLunch(lunchItem[0]);
       // Run select lunch spot function
-      self.selectedSpot(lunchItem[0]);
+      self.selectSpot(lunchItem[0]);
     } else {
       alert("No more options for you!");
     }
@@ -134,6 +153,21 @@ var ViewModel = function() {
     // Check item against excluded parameters
     // if so, return true
     // add item to this.lunchList
+  }
+
+  this.updatePrice = function(selected) {
+    if (self.excludeParams().price <= selected.price){
+
+    }
+    console.log("New price: " + self.excludeParams().price);
+    // console.log(self.lunchList()[self.lunchList().length-1]);
+  }
+
+  this.updateType = function(selected) {
+    if (!self.excludeParams().types.includes(selected.type)) {
+      self.excludeParams().types.push(selected.type);
+      console.log(self.excludeParams().types);
+    }
   }
 
 }
